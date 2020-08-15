7adb5441c894f3efe0cbcb23b5882f2de66b0776
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-15 18:17:23 +0900

    インスタンスの作成

--- Hand.java
+++ Hand.java
@@ -1,5 +1,13 @@
 package Janken01;
 
 public class Hand {
-  
+  private int hand;
+
+  public int getHand() {
+    return this.hand;
+  }
+
+  public void setHand(int hand) {
+    this.hand = hand;
+  }
 }
--- Main.java
+++ Main.java
@@ -2,6 +2,12 @@ package Janken01;
 
 public class Main {
   public static void main(String[] args) {
-  
+  Hand player = new Hand();
+  Hand computer = new Hand();
+  player.setHand(2);
+  computer.setHand(1);
+
+  System.out.println(player.getHand());
+  System.out.println(computer.getHand());
   }
 }
