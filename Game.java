c13b1fd2fc2d109fc0e74ba7daf7b3d34d0c529a
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-15 20:30:08 +0900

    あいこのときの処理

--- Game.java
+++ Game.java
@@ -1,4 +1,12 @@
 package Janken01;
 
 public class Game {
+  public static boolean draw(int player, int computer) {
+    if (player == computer) {
+      System.out.println("あいこです！");
+      return true;
+    } else {
+      return false;
+    }
+  }
 }
--- Main.java
+++ Main.java
@@ -6,10 +6,11 @@ public class Main {
     Hand computer = new Hand();
 
     System.out.println("じゃんけんを始めます！");
-    player.setPlayerHand();
-    computer.setComputerHand();
-
-    System.out.println("あなた：" + player.toString());
-    System.out.println("あいて：" + computer.toString());
+    do {
+      player.setPlayerHand();
+      computer.setComputerHand();
+      System.out.println("あなた：" + player.toString());
+      System.out.println("あいて：" + computer.toString());
+    }while(Game.draw(player.getHand(), computer.getHand()));
   }
 }
