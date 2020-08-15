ef9915cc6e67e6a1bddbcce59460d851fd5999c2
Perhashy <dr.hashy-hcr1002@ezweb.ne.jp>
2020-08-15 19:06:26 +0900

    標準入力で手を入力

--- Hand.java
+++ Hand.java
@@ -1,5 +1,7 @@
 package Janken01;
 
+import java.util.Scanner;
+
 public class Hand {
   private int hand;
 
@@ -9,6 +11,21 @@ public class Hand {
 
   public void setHand(int hand) {
     this.hand = hand;
+  }
+
+  public void setPlayerHand() {
+    Scanner sc = new Scanner(System.in);
+    System.out.println(
+      "手を数字で入力してください" + "\n" +
+      "1：グー、2：チョキ、3：パー"
+    );
+    int playerHand = sc.nextInt();
+    if (playerHand > 0 && playerHand <= 3) {
+      this.hand = playerHand;
+    } else {
+      System.out.println("正しくない手を出しました、あなたの負けです");
+      System.exit(0);
+    }
   }
 
   @Override
--- Main.java
+++ Main.java
@@ -2,12 +2,14 @@ package Janken01;
 
 public class Main {
   public static void main(String[] args) {
-  Hand player = new Hand();
-  Hand computer = new Hand();
-  player.setHand(2);
-  computer.setHand(1);
+    Hand player = new Hand();
+    Hand computer = new Hand();
 
-  System.out.println(player.toString());
-  System.out.println(computer.toString());
+    System.out.println("じゃんけんを始めます！");
+    player.setPlayerHand();
+    computer.setHand(1);
+
+    System.out.println("あなた：" + player.toString());
+    System.out.println("あいて：" + computer.toString());
   }
 }
